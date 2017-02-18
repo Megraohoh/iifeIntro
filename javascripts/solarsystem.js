a02291191a4c28e113eea4e369149d5a7d81ec37
Meghan Debity <mdebity@gmail.com>
2017-02-18 11:13:56 -0600

    aug- new files

--- javascripts/solarsystem.js
+++ javascripts/solarsystem.js
@@ -1,19 +1,10 @@
 var SolarSystem = (function(){
 
-	var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
 	var dwarfPlanets = ["Pluto"];
-	var numPlanetsPeopleLandedOn = 0;
+	
 
 	return {
-		getPlanets: function(){
-			return planets;
-		},
-		getPlanetsLandedPeopleOn: function(){
-			return numPlanetsPeopleLandedOn;
-		},
-		setPlanetsLandedPeopleOn: function(){
-			numPlanetsPeopleLandedOn++;
-		},
+		
 		getDwarfPlanets : function(){
 			return "they are rejects";
 		},
@@ -24,14 +15,7 @@ var SolarSystem = (function(){
 
 })();
 
-// console.log("Planets: ",SolarSystem.getPlanets());
 
-// console.log("Plants with people: ", SolarSystem.getPlanetsLandedPeopleOn());
-// SolarSystem.setSpaceships("Voyager 1");
-// SolarSystem.setSpaceships("Voyager 2");
-// SolarSystem.setSpaceships("Gemini");
-// SolarSystem.setSpaceships("Apollo");
-// console.log("Spaceships: ", SolarSystem.getSpaceships());
 
 
 
